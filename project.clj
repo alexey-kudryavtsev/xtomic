a137422c5847880c5664d0df46761181b031a714
kudryavtsev.alexey <kudryavtsev.alexey@tyrell.group>
2022-09-03 01:27:00 +0300

    fix typo

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.xtdb/xtdb-core "1.21.0"]]
   :repositories [["github" {:url "https://maven.pkg.github.com/alexey-kudryavtsev/xtomic"
-                            :username :env/github_actom
+                            :username :env/github_actor
                             :password :env/github_token}]]
   :profiles {:with-shim {:source-paths ["shim"]}})
