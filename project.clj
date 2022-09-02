915bf4d2ddf8ebd0021512ada0c1621e5c12424b
kudryavtsev.alexey <kudryavtsev.alexey@tyrell.group>
2022-09-03 01:23:07 +0300

    deploy action

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject xtdb-datomic-compat "0.1.0-SNAPSHOT"
+(defproject xtomic "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.11.1"]
-  [com.xtdb/xtdb-core "1.21.0"]]
+                 [com.xtdb/xtdb-core "1.21.0"]]
+  :repositories [["github" {:url "https://maven.pkg.github.com/alexey-kudryavtsev/xtomic"
+                            :username :env/github_actom
+                            :password :env/github_token}]]
   :profiles {:with-shim {:source-paths ["shim"]}})
